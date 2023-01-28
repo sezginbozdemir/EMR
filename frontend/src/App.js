d1114ac2cf662dadc0dffdbae4c31b79bebbf8d4
Atlas Kaplan <kxplxn@icloud.com>
2023-01-28 22:40:43 +0000

    Bunuda al aq

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,23 @@
 import React from 'react';
 
-const ev = {
-  boyut: "30metrekare",
-  katlar: 20
-}
-
-ev.boyut
-
-function Comp({ yazi }) {
+// reusable component
+function Comp({ arg }) {
   return (
     <div>
       <p>Comp</p>
-      <p>{yazi}</p>
+      <p>{arg}</p>
     </div>
-  );
+  )
 }
 
-function App() {
-  // const yazilar = ["yaziA", "yaziB", "yaziC"]
-  const yaziA = "yaziA";
+// data
+const stringArray = ["yaziA", "yaziB", "yaziC"]
 
-  // {yazilar.foreach(x => {
-  //   console.log(x)
-  //   return <comp yazi={x} />
-  // })}
+const App = () => (
+  <div>
+    <div>Sezzzzzzzz</div>
+    {stringArray.map(x => <Comp arg={x} />)}
+  </div>
+)
 
-  return (
-    <div>
-      <div>Sezzzzzzzz</div>
-      <Comp yazi={ev.boyut} />
-    </div>
-  );
-}
-
-export default App;
+export default App
