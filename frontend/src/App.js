3f209bb9dd172d4292929c8dbe3b612ec4a58101
sezginbozdemir <88796191+sezginbozdemir@users.noreply.github.com>
2023-01-29 12:13:32 +0200

    andreea

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,9 +15,17 @@ const stringArray = ["yaziA", "yaziB", "yaziC"]
 
 const App = () => (
   <div>
-    <div>Sezzzzzzzz</div>
+    <div></div>
     {stringArray.map(x => <Comp arg={x} />)}
   </div>
 )
 
 export default App
+
+export const andreea = 
+<>
+  <p id="andreea"> "CE FACI BAI" </p>
+  <button type= "button"
+ onclick={document.getElementById('andreea').innerHTML = 'MATA ANDREEA'}>dai </button>
+
+</>
